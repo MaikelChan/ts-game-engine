a7504b5b688898d4d0aa73229521df987e6c9663
MaikelChan <maikelchan88@gmail.com>
2020-02-03 03:39:09 +0100

    Implemented fly camera controls.

--- example1/Example1Scene.ts
+++ example1/Example1Scene.ts
@@ -1,6 +1,9 @@
-import { Scene, Materials, Meshes, Entities, Game, Texture2D } from "ts-game-engine";
-import { vec4, vec3, quat } from "gl-matrix";
+import { Scene, Materials, Meshes, Entities, Game, Texture2D, InputSystem, Keys } from "ts-game-engine";
+import { vec4, vec3, quat, vec2 } from "gl-matrix";
 import { IDisposable } from "ts-game-engine/lib/Interfaces";
+
+const MOUSE_SENSITIVITY: number = 0.15;
+const MOVEMENT_SPEED: number = 10;
 
 export class Example1Scene extends Scene {
 
@@ -19,6 +22,8 @@ export class Example1Scene extends Scene {
     private lightMesh: Meshes.SphereMesh;
 
     private customLights: CustomLight[];
+
+    private cameraAngles: vec3;
 
     constructor(game: Game) {
         super(game);
@@ -64,6 +69,8 @@ export class Example1Scene extends Scene {
             new CustomLight(this, this.lightMesh, vec3.fromValues(-1, 0, 8), vec3.fromValues(0.6, 1, 0.2), 4),
             new CustomLight(this, this.lightMesh, vec3.fromValues(1, 4, -2), vec3.fromValues(1, 0.2, 0.4), 4)
         ];
+
+        this.cameraAngles = vec3.fromValues(-20, 0, 0);
     }
 
     public Start(): void {
@@ -85,18 +92,25 @@ export class Example1Scene extends Scene {
         this.AddEntity(this.monkeyRenderer1);
         this.AddEntity(this.monkeyRenderer2);
         this.AddEntity(this.monkeyRenderer3);
+
+        this.Camera.Transform.Position = vec3.fromValues(0, 4.5, 15);
+
+        let cameraRotation: quat = quat.create();
+        this.Camera.Transform.Rotation = quat.fromEuler(cameraRotation, this.cameraAngles[0], this.cameraAngles[1], this.cameraAngles[2]);
     }
 
     public Update(deltaTime: number): void {
         super.Update(deltaTime);
 
+        this.ProcessInput(deltaTime);
+
         let currentTime = this.Game.ElapsedSeconds;
-        let speed = currentTime * 0.25 * Math.PI;
+        //let speed = currentTime * 0.25 * Math.PI;
 
-        this.Camera.Transform.Position = vec3.fromValues(Math.sin(speed) * 15, 4.5, Math.cos(speed) * 15);
+        // this.Camera.Transform.Position = vec3.fromValues(Math.sin(speed) * 15, 4.5, Math.cos(speed) * 15);
 
-        let cameraRotation: quat = quat.create();
-        this.Camera.Transform.Rotation = quat.fromEuler(cameraRotation, -20, currentTime * 0.25 * 180, 0);
+        // let cameraRotation: quat = quat.create();
+        // this.Camera.Transform.Rotation = quat.fromEuler(cameraRotation, -20, currentTime * 0.25 * 180, 0);
 
         let monkey2Rotation: quat = quat.create();
         this.monkeyRenderer2.Transform.Rotation = quat.fromEuler(monkey2Rotation, 0, currentTime * 180, 0);
@@ -119,6 +133,53 @@ export class Example1Scene extends Scene {
         for (let l: number = 0; l < this.customLights.length; l++) this.customLights[l].Dispose();
 
         super.Dispose();
+    }
+
+    private ProcessInput(deltaTime: number): void {
+        const input: InputSystem.InputSystem = this.Game.InputSystem;
+
+        // Keyboard movement
+
+        const direction: vec3 = vec3.create();
+
+        if (input.GetKey(Keys.KEY_W)) direction[2] = 1;
+        else if (input.GetKey(Keys.KEY_S)) direction[2] = -1;
+
+        if (input.GetKey(Keys.KEY_A)) direction[0] = -1;
+        else if (input.GetKey(Keys.KEY_D)) direction[0] = 1;
+
+        if (input.GetKey(Keys.KEY_Q)) direction[1] = -1;
+        else if (input.GetKey(Keys.KEY_E)) direction[1] = 1;
+
+        const offset: vec3 = vec3.create();
+        const temp: vec3 = vec3.create();
+
+        vec3.add(offset, offset, vec3.scale(temp, this.Camera.Transform.Right, direction[0]));
+        vec3.add(offset, offset, vec3.scale(temp, this.Camera.Transform.Forward, direction[2]));
+        offset[1] += direction[1];
+        vec3.normalize(offset, offset);
+
+        let finalSpeed: number;
+        if (input.GetKey(Keys.KEY_SHIFT_LEFT) || input.GetKey(Keys.KEY_SHIFT_RIGHT))
+            finalSpeed = MOVEMENT_SPEED * 3;
+        else
+            finalSpeed = MOVEMENT_SPEED;
+
+        vec3.scale(temp, offset, finalSpeed * deltaTime);
+        this.Camera.Transform.Move(temp);
+
+        // Mouse rotation
+
+        if (input.GetMouseButton(Keys.MOUSE_BUTTON_MAIN)) {
+
+            const delta: vec2 = input.MouseDeltaMovement;
+
+            this.cameraAngles[0] -= delta[1] * MOUSE_SENSITIVITY;
+            this.cameraAngles[1] -= delta[0] * MOUSE_SENSITIVITY;
+            this.cameraAngles[2] = 0;
+
+            this.Camera.Transform.SetEulerAngles(this.cameraAngles[0], this.cameraAngles[1], this.cameraAngles[2]);
+        }
     }
 }
 
